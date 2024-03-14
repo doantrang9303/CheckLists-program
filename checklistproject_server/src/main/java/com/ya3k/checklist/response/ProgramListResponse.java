d1001a92676a63cbe08d7d7496061236dcd47fa1
ya3k <truongdinhvan1233@gmail.com>
2024-03-14 16:17:55 +0700

    a
    a

---
+++ checklistproject_server/src/main/java/com/ya3k/checklist/response/ProgramListResponse.java
@@ -0,0 +1,23 @@
+package com.ya3k.checklist.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ya3k.checklist.repository.ProgramRepository;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class ProgramListResponse {
+    @JsonProperty("program_list")
+    public List<ProgramResponse> programResponseList;
+
+    @JsonProperty( "total")
+    public int total;
+    @JsonProperty("total_page")
+    public int totalPage;
+}
