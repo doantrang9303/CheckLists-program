6f2f30dce574e26ad180a7a1b9c794c3da59c6be
doantrang9303 <doannguyenhuyentrang9303@gmail.com>
2024-03-15 16:02:50 +0700

    Create new task

--- checklistproject_server/src/main/java/com/ya3k/checklist/entity/Tasks.java
+++ checklistproject_server/src/main/java/com/ya3k/checklist/entity/Tasks.java
@@ -1,35 +1,50 @@
 package com.ya3k.checklist.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+@Table(name = "tasks")
 @Data
-
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tasks {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name")
-    private String taskName;
+    private String name;
 
     @ManyToOne
     @JsonBackReference
     @JoinColumn(name = "program_id")
     private Program program;
 
-    @Column(name = "status")
+    @Column(name="status")
     private String status;
 
-    @Column(name = "create_time")
-    private LocalDate createTime;
+    @Column(name="create_time")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @CreatedDate
+    private LocalDate create_time;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @Column(name = "end_time")
-    private Date endTime;
+    @Column(name="end_time")
+    @DateTimeFormat(pattern = "yyyy-MM-dd ")
+    private LocalDate end_time;
+
+
+
+
 }
