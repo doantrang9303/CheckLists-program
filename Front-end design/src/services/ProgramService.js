d5f4ea6f343820b936e979c77c6d0c43a692f25c
ducsangtran <145980048+ducsangtran@users.noreply.github.com>
2024-03-20 00:01:53 +0700

    filter program

--- Front-end design/src/services/ProgramService.js
+++ Front-end design/src/services/ProgramService.js
@@ -1,8 +1,13 @@
 import axios from './customize-axios';
 
 
+<<<<<<< Updated upstream
 const fetchAllProgram = (page,username ) => { 
   return axios.get(`/programs/all?page=${page}`, {
+=======
+const fetchAllProgram = (page,username) => { 
+  return axios.get(`/programs?page=${page}`, {
+>>>>>>> Stashed changes
     headers: {
       // Include the username in the request headers if available
       'user_name': username || ''
@@ -16,9 +21,31 @@ const createProgram = (programData, username) => {
     }
   }); 
 }
+<<<<<<< Updated upstream
 const ProgramService = {
   fetchAllProgram,
   createProgram,
+=======
+
+
+const filterProgramByStatus = (status, username) => {
+  return axios.get(`/programs?status=${status}`, 
+  {
+    headers: {
+      'user_name': username || ''
+    }
+  }); 
+}
+const deleteProgram = (programId) => {
+  return axios.delete(`/programs/delete/${programId}`);
+}
+const ProgramService = {
+  fetchAllProgram,
+  createProgram,
+  deleteProgram,
+  filterProgramByStatus
+  
+>>>>>>> Stashed changes
 }
 
 export  default ProgramService;
