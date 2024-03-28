8ead3dd22bff84bf250e9af10c2fae575a1133a6
doantrang9303 <doannguyenhuyentrang9303@gmail.com>
2024-03-28 15:30:13 +0700

    BE_add exception check format dateTime in ApplicationExceptionHandler

--- back_end/src/main/java/com/ya3k/checklist/controlleradvice/ApplicationExceptionHandler.java
+++ back_end/src/main/java/com/ya3k/checklist/controlleradvice/ApplicationExceptionHandler.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+import java.time.format.DateTimeParseException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -37,6 +37,13 @@ public class ApplicationExceptionHandler {
         return errors;  // Return the errors map with categorized error lists
     }
 
-
+   //   Exception handler for handling DateTimeFormat parsing errors
+    @ExceptionHandler(DateTimeParseException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleDateTimeFormatException(DateTimeParseException e) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", "Invalid date format for endTime, please use yyyy-MM-dd format.");
+        return error;
+    }
 
 }
