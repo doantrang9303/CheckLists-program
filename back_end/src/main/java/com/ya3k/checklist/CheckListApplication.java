685f734794b9eaea95655b16d70639a0076eed7c
doantrang9303 <doannguyenhuyentrang9303@gmail.com>
2024-04-11 08:24:45 +0700

    test setup variable environment in backend.env and email.env

--- back_end/src/main/java/com/ya3k/checklist/CheckListApplication.java
+++ back_end/src/main/java/com/ya3k/checklist/CheckListApplication.java
@@ -2,10 +2,14 @@ package com.ya3k.checklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@Configuration
+@PropertySource("classpath:backend.env")
 public class CheckListApplication {
 
     public static void main(String[] args) {
