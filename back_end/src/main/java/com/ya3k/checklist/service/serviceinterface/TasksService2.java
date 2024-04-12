27b7e9318cb801b4465cdc1c985802cfb9bd2640
ya3k <truongdinhvan1233@gmail.com>
2024-04-12 12:06:50 +0700

    s
    s

--- back_end/src/main/java/com/ya3k/checklist/service/serviceinterface/TasksService2.java
+++
@@ -1,8 +0,0 @@
-package com.ya3k.checklist.service.serviceinterface;
-
-import com.ya3k.checklist.dto.TasksDto;
-
-public interface TasksService2 {
-    TasksDto createTask(TasksDto taskDto, Integer programId);
-
-}
