076175e5e1bb6a21c302a2d77830f2f7b7598ebe
ducsangtran <145980048+ducsangtran@users.noreply.github.com>
2024-04-15 11:08:07 +0700

    update

---
+++ front-end/src/services/UserService.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const addUser = async (userName, email) => {
+    try {
+        const response = await axios.post(
+            process.env.REACT_APP_SERVER_URL + "/users/add",
+            null,
+            {
+                headers: {
+                    userName: userName,
+                    email: email,
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        throw error.response.data;
+    }
+};
+
+const UserService = {
+    addUser,
+};
+
+export default UserService;
